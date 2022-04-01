9071bfdffdb82dae2452f06934b5f050fc626f97
anggi dwi darmawan <pratamaanggi090@gmail.com>
2022-04-01 12:37:48 +0700

    membuat list view

--- list_view/lib/main.dart
+++ list_view/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:list_view/list_view.dart';
+import 'package:list_view/listo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyListView(),
+      home: Listo(),
     );
   }
 }
